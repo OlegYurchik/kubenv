5aff43ab172b7c9a9a4a3e1d09e78d8b907003f8
Oleg Yurchik <oleg@yurchik.space>
2023-01-21 20:49:47 +0300

    Add commands

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 use std::cmp::{Ordering, PartialOrd};
 use std::fs;
 use std::path::PathBuf;
+use std::result;
 
+use hashbrown::HashMap;
 use home;
 use sha256;
 
-fn get_default_kube_dir() -> Result<PathBuf, String> {
+pub type Result<T = ()> = result::Result<T, String>;
+
+fn get_default_kube_dir() -> Result<PathBuf> {
     let home_dir = match home::home_dir() {
         Some(path) => path.to_path_buf(),
         None => return Err(String::from("Cannot get home directory")),
@@ -15,38 +19,44 @@ fn get_default_kube_dir() -> Result<PathBuf, String> {
     return Ok(kube_dir);
 }
 
-fn get_default_kubeman_dir() -> Result<PathBuf, String> {
+fn get_default_kubeman_dir() -> Result<PathBuf> {
     let kube_dir = get_default_kube_dir()?;
     let kubeman_dir = kube_dir.join("kubeman");
     return Ok(kubeman_dir);
 }
 
+fn get_file_hash(path: &PathBuf) -> Result<String> {
+    let hash = match sha256::try_digest(path.as_path()) {
+        Ok(bytes) => bytes,
+        Err(msg) => match path.to_str() {
+            Some(ps) => {
+                return Err(format!("Cannot get hash from file '{}': {}", ps, msg));
+            }
+            None => return Err(format!("Cannot get hash from file: {}", msg)),
+        },
+    };
+
+    return Ok(hash);
+}
+
 #[derive(PartialEq)]
 pub struct KubeConfig {
-    name: Option<String>,
+    name: String,
     path: PathBuf,
     hash: String,
 }
 
 impl KubeConfig {
-    pub fn new(path: PathBuf, name: Option<String>) -> Result<Self, String> {
-        let hash = match sha256::try_digest(path.as_path()) {
-            Ok(bytes) => bytes,
-            Err(_) => match path.to_str() {
-                Some(ps) => {
-                    return Err(format!(
-                        "Cannot read and/or getting hash from file '{}'",
-                        ps,
-                    ));
-                }
-                None => return Err(String::from("Cannot get PathBuf str")),
-            },
+    pub fn new(path: PathBuf, hash: String, name: Option<String>) -> Self {
+        let name = match name {
+            Some(n) => n,
+            None => String::from(&hash[..8]),
         };
 
-        return Ok(Self { name, path, hash });
+        return Self { name, path, hash };
     }
 
-    pub fn name(&self) -> &Option<String> {
+    pub fn name(&self) -> &str {
         return &self.name;
     }
 
@@ -55,27 +65,33 @@ impl KubeConfig {
     }
 }
 
-impl PartialOrd for KubeConfig {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        if let Some(name_1) = self.name() {
-            if let Some(name_2) = other.name() {
-                return Some(name_1.cmp(name_2));
-            }
-        }
-
-        return Some(self.hash().cmp(other.hash()));
+impl Clone for KubeConfig {
+    fn clone(&self) -> Self {
+        return Self {
+            name: self.name.clone(),
+            hash: self.hash.clone(),
+            path: self.path.clone(),
+        };
     }
 }
 
-pub struct KubeManConfig {
+impl PartialOrd for KubeConfig {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        return Some(self.name().cmp(other.name()));
+    }
+}
+
+pub struct KubeMan {
     kube_dir: PathBuf,
     kubeman_dir: PathBuf,
     current_config: Option<KubeConfig>,
     configs: Vec<KubeConfig>,
+    configs_by_name: HashMap<String, KubeConfig>,
+    configs_by_hash: HashMap<String, KubeConfig>,
 }
 
-impl KubeManConfig {
-    pub fn new(kubeman_dir: Option<PathBuf>, kube_dir: Option<PathBuf>) -> Result<Self, String> {
+impl KubeMan {
+    pub fn new(kubeman_dir: Option<PathBuf>, kube_dir: Option<PathBuf>) -> Result<Self> {
         let kube_dir = match kube_dir {
             Some(path_buf) => path_buf,
             None => get_default_kube_dir()?,
@@ -84,51 +100,135 @@ impl KubeManConfig {
             Some(path_buf) => path_buf,
             None => get_default_kubeman_dir()?,
         };
-        let mut kubemanconfig = Self {
+        let kubeman = Self {
             kube_dir,
             kubeman_dir,
             current_config: None,
             configs: vec![],
+            configs_by_name: HashMap::new(),
+            configs_by_hash: HashMap::new(),
         };
-        kubemanconfig.sync()?;
-        return Ok(kubemanconfig);
+        return Ok(kubeman);
+    }
+
+    pub fn current_config(&self) -> Option<&KubeConfig> {
+        return self.current_config.as_ref();
     }
 
     pub fn configs(&self) -> Vec<&KubeConfig> {
         return self.configs.iter().collect();
     }
 
-    pub fn current_config(&self) -> &Option<KubeConfig> {
-        return &self.current_config;
+    pub fn get_config_by_name(&self, name: &str) -> Option<&KubeConfig> {
+        return self.configs_by_name.get(name);
     }
 
-    fn sync(&mut self) -> Result<(), String> {
-        let kubeman_dir = self.kube_dir.join("kubeman");
-        if !kubeman_dir.is_dir() {
-            if let Err(_) = fs::create_dir_all(kubeman_dir.as_path()) {
-                match kubeman_dir.to_str() {
-                    Some(path) => return Err(format!("Cannot create directory '{}'", path)),
-                    None => return Err(String::from("Cannot create directory")),
-                };
-            };
+    pub fn get_config_by_hash(&self, hash: &str) -> Option<&KubeConfig> {
+        return self.configs_by_hash.get(hash);
+    }
+
+    pub fn apply(&self, name: &str) -> Result {
+        let kubeconfig = match self.get_config_by_name(name) {
+            Some(kc) => kc,
+            None => return Err(format!("Cannot find config with name '{}'", name)),
         };
 
-        self.update_configs(&kubeman_dir)?;
+        let config_file = self.kube_dir.join("config");
+        if get_file_hash(&config_file)? == kubeconfig.hash {
+            return Err(format!("Config '{}' already applied", name));
+        }
+        if let Err(msg) = fs::copy(&kubeconfig.path, config_file) {
+            return Err(format!(
+                "Cannot apply config with name '{}': {}",
+                kubeconfig.name, msg,
+            ));
+        }
+
+        return Ok(());
+    }
+
+    pub fn import(&self, content: &[u8], name: Option<String>) -> Result {
+        let hash = sha256::digest(content);
+        if let Some(kc) = self.configs_by_hash.get(&hash) {
+            return Err(format!("Config already exists with name: '{}'", &kc.name));
+        }
+        let name = match name {
+            Some(n) => {
+                if let Some(kc) = self.configs_by_name.get(&n) {
+                    return Err(format!("Config with name '{}' already exists", kc.name));
+                }
+                n
+            }
+            None => hash.clone(),
+        };
+
+        let mut kubeconfig_filename = name.clone();
+        kubeconfig_filename.push_str(".kubeconfig");
+        let kubeconfig_path = self.kubeman_dir.join(kubeconfig_filename);
+        if let Err(msg) = fs::write(&kubeconfig_path, content) {
+            match kubeconfig_path.to_str() {
+                Some(p) => return Err(format!("Cannot write to file '{}': {}", p, msg)),
+                None => return Err(format!("Cannot write to file: {}", msg)),
+            }
+        };
+
+        return Ok(());
+    }
+
+    pub fn remove(&self, name: String) -> Result {
+        let kubeconfig = match self.configs_by_name.get(&name) {
+            Some(kc) => kc,
+            None => return Err(format!("Cannot find config with name '{}'", name)),
+        };
+
+        if let Err(msg) = fs::remove_file(&kubeconfig.path) {
+            return Err(format!(
+                "Cannot remove config with name '{}': {}",
+                kubeconfig.name, msg,
+            ));
+        }
+
+        return Ok(());
+    }
+
+    pub fn sync(&mut self) -> Result {
+        if !self.kubeman_dir.is_dir() {
+            if let Err(msg) = fs::create_dir_all(self.kubeman_dir.as_path()) {
+                match self.kubeman_dir.to_str() {
+                    Some(path) => {
+                        return Err(format!(
+                            "Cannot create kubeman directory '{}': {}",
+                            path, msg,
+                        ))
+                    }
+                    None => return Err(format!("Cannot create kubeman directory: {}", msg)),
+                }
+            }
+        };
+
+        self.update_configs()?;
         self.update_current_config()?;
 
         return Ok(());
     }
 
-    fn update_configs(&mut self, kubeman_dir: &PathBuf) -> Result<(), String> {
-        let config_files = match fs::read_dir(kubeman_dir.clone()) {
+    fn update_configs(&mut self) -> Result {
+        let config_files = match fs::read_dir(&self.kubeman_dir) {
             Ok(value) => value,
-            Err(_) => match kubeman_dir.to_str() {
-                Some(path) => return Err(format!("Cannot read files from directory '{}'", path)),
-                None => return Err(String::from("Cannot read files from kubeman directory")),
+            Err(msg) => match self.kubeman_dir.to_str() {
+                Some(path) => {
+                    return Err(format!(
+                        "Cannot read files from directory '{}': {}",
+                        path, msg,
+                    ))
+                }
+                None => return Err(format!("Cannot read files from kubeman directory: {}", msg)),
             },
         };
 
         self.configs.clear();
+        self.configs_by_name.clear();
+        self.configs_by_hash.clear();
         for config_file in config_files {
             let config_file = match config_file {
                 Ok(cf) => cf,
@@ -144,19 +244,12 @@ impl KubeManConfig {
                     Some(s) => String::from(s),
                     None => file_name.clone(),
                 };
-                match KubeConfig::new(path, Some(name)) {
-                    Ok(kubeconfig) => {
-                        let len = self.configs.len();
-                        let mut index = 0;
-                        if len > 0 {
-                            index = len - 1;
-                            while index > 0 && kubeconfig < self.configs[index - 1] {
-                                index -= 1;
-                            }
-                        }
-                        self.configs.insert(index, kubeconfig);
-                    }
+                let hash = match get_file_hash(&path) {
+                    Ok(h) => h,
                     Err(_) => continue,
+                };
+                if let Err(_) = self.add(KubeConfig::new(path, hash, Some(name))) {
+                    continue;
                 };
             };
         }
@@ -164,12 +257,46 @@ impl KubeManConfig {
         return Ok(());
     }
 
-    fn update_current_config(&mut self) -> Result<(), String> {
+    fn update_current_config(&mut self) -> Result {
         let current_config_file = self.kube_dir.join("config");
+        let hash = get_file_hash(&current_config_file)?;
 
         if current_config_file.is_file() {
-            self.current_config = Some(KubeConfig::new(current_config_file, None)?);
+            self.current_config = Some(KubeConfig::new(current_config_file, hash, None));
         }
+
+        return Ok(());
+    }
+
+    fn add(&mut self, kubeconfig: KubeConfig) -> Result {
+        if let Some(kc) = self.configs_by_name.get(&kubeconfig.name) {
+            return Err(format!("Config with name '{}' already exists", kc.name));
+        }
+        if let Some(kc) = self.configs_by_hash.get(&kubeconfig.hash) {
+            return Err(format!("Config already exists with name '{}'", kc.name));
+        }
+
+        // Add to self.configs
+        let kubeconfig_tmp = kubeconfig.clone();
+        let len = self.configs.len();
+        let mut index = 0;
+        if len > 0 {
+            index = len - 1;
+            while index > 0 && kubeconfig_tmp < self.configs[index - 1] {
+                index -= 1;
+            }
+        }
+        self.configs.insert(index, kubeconfig_tmp);
+
+        // Add to self.configs_by_name
+        let kubeconfig_tmp = kubeconfig.clone();
+        self.configs_by_name
+            .insert(kubeconfig_tmp.name.clone(), kubeconfig_tmp);
+
+        // Add to self.configs_by_hash
+        let kubeconfig_tmp = kubeconfig.clone();
+        self.configs_by_hash
+            .insert(kubeconfig_tmp.hash.clone(), kubeconfig_tmp);
 
         return Ok(());
     }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+use std::fs;
 use std::path::PathBuf;
 
 use clap::{Parser, Subcommand};
-use kubeman::KubeManConfig;
+use kubeman::{KubeMan, Result};
 
 #[derive(Parser)]
 #[command(name = "KubeMan")]
@@ -19,30 +20,55 @@ struct Cli {
 #[derive(Subcommand)]
 enum Commands {
     List,
+    Apply {
+        name: String,
+    },
+    Remove {
+        name: String,
+    },
+    Import {
+        #[arg(short, long)]
+        name: Option<String>,
+        #[arg(short, long)]
+        file: Option<PathBuf>,
+    },
+}
+
+fn print_error(message: String) {
+    eprintln!("[ERROR] {}", message);
 }
 
 fn main() {
     let cli = Cli::parse();
 
-    let kubemanconfig = match KubeManConfig::new(cli.dir, cli.kube_dir) {
-        Ok(kmc) => kmc,
-        Err(msg) => panic!("{}", msg),
+    let mut kubeman = match KubeMan::new(cli.dir, cli.kube_dir) {
+        Ok(km) => km,
+        Err(msg) => {
+            print_error(msg);
+            return;
+        }
     };
+    if let Err(msg) = kubeman.sync() {
+        print_error(msg);
+        return;
+    }
 
-    match &cli.command {
-        Some(Commands::List) => list(&kubemanconfig),
-        None => (),
+    let result = match &cli.command {
+        Some(Commands::List) => list(kubeman),
+        Some(Commands::Apply { name }) => apply(kubeman, name.clone()),
+        Some(Commands::Import { name, file }) => import(kubeman, name.clone(), file.clone()),
+        Some(Commands::Remove { name }) => remove(kubeman, name.clone()),
+        None => Ok(()),
+    };
+    if let Err(msg) = result {
+        print_error(msg);
     }
 }
 
-fn list(kubemanconfig: &KubeManConfig) {
-    let current_config = kubemanconfig.current_config();
-    for kubeconfig in kubemanconfig.configs() {
-        let hash = kubeconfig.hash();
-        let name = match kubeconfig.name().as_ref() {
-            Some(n) => String::from(n),
-            None => String::from(&hash[..8]),
-        };
+fn list(kubeman: KubeMan) -> Result {
+    let current_config = kubeman.current_config();
+    for kubeconfig in kubeman.configs() {
+        let name = kubeconfig.name();
         let mut output = format!("  {}", name);
         if let Some(cf) = current_config {
             if cf.hash() == kubeconfig.hash() {
@@ -51,4 +77,43 @@ fn list(kubemanconfig: &KubeManConfig) {
         }
         println!("{}", output);
     }
+
+    return Ok(());
+}
+
+fn apply(kubeman: KubeMan, name: String) -> Result {
+    kubeman.apply(&name)?;
+    println!("Apply config '{}' succesfully", name);
+
+    return Ok(());
+}
+
+fn import(kubeman: KubeMan, name: Option<String>, path: Option<PathBuf>) -> Result {
+    let content: Vec<u8> = match path {
+        Some(path) => match fs::read(&path) {
+            Ok(c) => c,
+            Err(msg) => {
+                match path.to_str() {
+                    Some(p) => return Err(format!("Cannot read file '{}': {}", p, msg)),
+                    None => return Err(format!("Cannot read file: {}", msg)),
+                };
+            }
+        },
+        None => {
+            let content: Vec<u8> = Vec::new();
+            content
+        }
+    };
+    kubeman.import(&content, name)?;
+    // TODO: Need add name to println
+    println!("Import config succesfully");
+
+    return Ok(());
+}
+
+fn remove(kubeman: KubeMan, name: String) -> Result {
+    kubeman.remove(name.clone())?;
+    println!("Remove config '{}' successfully", name);
+
+    return Ok(());
 }
